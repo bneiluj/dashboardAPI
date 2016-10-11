7c5fe15e40fdf597bc44b6959ae6a298c212fc60
bneiluj <julien.bouteloup@gmail.com>
2016-10-11 16:54:37 +0100

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:5050',
+    'webpack/hot/only-dev-server',
+    './index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel-loader'],
+      exclude: /node_modules/,
+      include: __dirname
+    },
+    {
+        test: /\.css$/,
+        loaders: [
+          'style', 'css',
+        ],
+    }]
+  }
+};
